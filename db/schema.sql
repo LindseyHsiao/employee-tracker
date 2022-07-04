35b581d32115a456fcd03daf017c3da34d296a00
Lindsey Sherrill <lindsey350@aol.com>
2022-07-03 23:39:00 -0400

    routes added

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,15 @@ CREATE TABLE role (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT --add foreign key 
+  department_id INT   
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT, --add foreign key
+  manager_id INT --add foreign key to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 );
 
